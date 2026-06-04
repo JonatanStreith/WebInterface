24953abe2470e9b8d60066387757fc397668fba5
Jonatan Streith <jonatan@streith.se>
2026-06-04 16:48:09 +0200

    Requirements

--- WebInterface.API/Models/CharacterCreationDto.cs
+++ WebInterface.API/Models/CharacterCreationDto.cs
@@ -1,8 +1,13 @@
-﻿namespace WebInterface.API.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebInterface.API.Models
 {
     public class CharacterCreationDto
     {
+        [Required(ErrorMessage = "No nameless characters")]
+        [MaxLength(50)]
         public string Name { get; set; } = string.Empty;
+        [MaxLength(250)]
         public string? Description { get; set; }
 
     }
